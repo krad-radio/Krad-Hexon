1d28f69bd6c84b2c9f049cf4d3b7aeba2e65b354
David Richards <rawdod@gmail.com>
2012-03-17 15:17:23 -0400

    remove extra deps from compile script

--- compile_krad_hexon_test.sh
+++ compile_krad_hexon_test.sh
@@ -1,1 +1,1 @@
-gcc -Wall krad_hexon_test.c krad_hexon.c -o krad_hexon_test -I. `pkg-config --libs --cflags xcb x11 gl xext xcb-atom cairo`
+gcc -Wall krad_hexon_test.c krad_hexon.c -o krad_hexon_test -I. `pkg-config --libs --cflags cairo`
